936706a955c12594ffcafdec0e9643bff49f896a
YI-CHENG <yich_liao@mail.davicom.com.tw>
2023-02-01 17:06:27 +0800

    package.json update for win package.json.linux.json for linux 使用不同檔案.

--- server/app.js
+++ server/app.js
@@ -14,6 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
+console.log( __dirname );
+console.log( __filename );
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
