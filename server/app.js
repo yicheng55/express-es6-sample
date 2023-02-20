a127f439f0d833a4d9485708ed09d44ace3df171
YI-CHENG <yich_liao@mail.davicom.com.tw>
2023-02-20 15:02:11 +0800

    .eslintrc.js added.

--- server/app.js
+++ server/app.js
@@ -1,17 +1,19 @@
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
 
-import catalogRouter from './src/routes/catalog.routes.js';  //Import routes for "catalog" area of site
-import productRouter from './src/routes/product.routes.js';  //Import routes for "catalog" area of site
-import flds_userRouter from './src/routes/flds_user.routes.js';
+import catalogRouter from "./src/routes/catalog.routes.js";  //Import routes for "catalog" area of site
+import productRouter from "./src/routes/product.routes.js";  //Import routes for "catalog" area of site
+import flds_userRouter from "./src/routes/flds_user.routes.js";
 // import * as userConfig from './src/config/user.config.js';
-import {userconfig as userConfig} from './src/config/user.config.js';
+import {userconfig as userConfig} from "./src/config/user.config.js";
 console.log(userConfig);
+
+// eslint-disable-next-line no-undef
 global.userConfig = userConfig;
 // console.log(global);
 
@@ -21,23 +23,25 @@ global.userConfig = userConfig;
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join( __dirname , "../public")));
 app.use(express.json());
 
 // console.log( __dirname );
 // console.log( __filename );
 
-app.use('/', indexRouter);
-app.use('/user', usersRouter);
+app.use("/", indexRouter);
+app.use("/user", usersRouter);
 
-app.use('/catalog/user', flds_userRouter);  // Add product routes to middleware chain.
-app.use('/catalog/product', productRouter);  // Add product routes to middleware chain.
+app.use("/catalog/user", flds_userRouter);  // Add product routes to middleware chain.
+app.use("/catalog/product", productRouter);  // Add product routes to middleware chain.
 // 最後放'/catalog'才不會被攔截
-app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+app.use("/catalog", catalogRouter);  // Add catalog routes to middleware chain.
 
 
 export default app;
